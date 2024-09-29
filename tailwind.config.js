b3f3804ea44470659079f803dd88959873d420e6
Miles Mosweu <tmtmosweu@gmail.com>
2024-09-29 16:27:24 +0200

    Add sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF5252',
+        secondary: '#FFD740',
+      },
+    },
   },
   plugins: [],
 };
