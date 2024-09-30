899c2372904e7bd9ff09004adfe03f8631e0e608
Miles Mosweu <tmtmosweu@gmail.com>
2024-10-01 00:32:23 +0200

    Refactor ProductList component to dynamically render products based on data from useProducts hook and handle cart actions using useCart hook

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,33 @@
+import { ReactElement } from 'react';
+
+import useCart from '../hooks/useCart';
+import useProducts from '../hooks/useProducts';
+
+import Product from './Product';
+
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const { dispatch, REDUCER_ACTIONS, cart } = useCart();
+  const { products } = useProducts();
+
+  let componentContent: ReactElement | ReactElement[] = <div>Loading...</div>;
+
+  if (products?.length) {
+    componentContent = products.map((product) => {
+      const inCart: boolean = cart.some((cartItem) => cartItem.sku === product.sku);
+
+      return (
+        <Product
+          key={product.sku}
+          product={product}
+          dispatch={dispatch}
+          REDUCER_ACTIONS={REDUCER_ACTIONS}
+          inCart={inCart}
+        />
+      );
+    });
+  }
+
+  return <ul>{componentContent}</ul>;
 };
 
 export default ProductList;
