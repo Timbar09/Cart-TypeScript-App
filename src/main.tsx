d4a8740eafa3cc9b2709ff0fea54339eb365bb53
Miles Mosweu <tmtmosweu@gmail.com>
2024-10-01 00:28:00 +0200

    Refactor main.tsx to wrap the App component with ProductsProvider and CartProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import { ProductsProvider } from './context/ProductsProvider.tsx';
+import { CartProvider } from './context/CartProvider.tsx';
+
 import App from './App.tsx';
+
 import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ProductsProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductsProvider>
   </StrictMode>
 );
