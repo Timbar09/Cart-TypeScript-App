d80b5a4eb6591197afb9d24f5c1a4917f7fefa8d
Miles Mosweu <tmtmosweu@gmail.com>
2024-10-01 00:28:33 +0200

    Refactor ProductsProvider to initialize products with data from JSON file

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,6 @@
-import { createContext, ReactElement, useState, useEffect } from 'react';
+import { createContext, ReactElement } from 'react';
+
+import { products } from '../../data/products.json';
 
 export type ProductType = {
   sku: string;
@@ -6,7 +8,7 @@ export type ProductType = {
   price: number;
 };
 
-const initState: ProductType[] = [];
+const initState: ProductType[] = products;
 
 export type useProductsContextType = { products: ProductType[] };
 
@@ -17,24 +19,9 @@ const ProductsContext = createContext<useProductsContextType>(initContextState);
 type ChildrenType = { children?: ReactElement | ReactElement[] };
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
-  const [products, setProducts] = useState<ProductType[]>(initState);
-
-  useEffect(() => {
-    const fetchProducts = async (): Promise<ProductType[]> => {
-      const response = await fetch('https://localhost:3500/products');
-      const data = await response.json().catch((error) => {
-        if (error instanceof Error) {
-          console.error('Error:', error.message);
-        }
-      });
-
-      return data;
-    };
-
-    fetchProducts().then((data) => setProducts(data));
-  }, []);
-
-  return <ProductsContext.Provider value={{ products }}>{children}</ProductsContext.Provider>;
+  return (
+    <ProductsContext.Provider value={{ products: initState }}>{children}</ProductsContext.Provider>
+  );
 };
 
 export default ProductsContext;
