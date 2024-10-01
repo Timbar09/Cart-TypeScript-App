9fad50f07659ab9ce2a2a607a033bba4fe34e315
Miles Mosweu <tmtmosweu@gmail.com>
2024-10-01 17:44:33 +0200

    Refactor Button component to add disabled attribute

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,7 @@
  * @param {React.ReactNode} children - Button content to display
  * @param {string} [className=''] - Additional classes to apply to button
  * @param {string} [type='button'] - Button type attribute
+ * @param {boolean} [disabled=false] - Button disabled attribute
  *
  * @returns {JSX.Element} - Button component
  */
@@ -13,14 +14,22 @@ type ButtonProps = {
   children: React.ReactNode;
   className?: string;
   type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
 };
 
-const Button = ({ handleClick, children, className = '', type = 'button' }: ButtonProps) => {
+const Button = ({
+  handleClick,
+  children,
+  className = '',
+  type = 'button',
+  disabled = false,
+}: ButtonProps): JSX.Element => {
   return (
     <button
       type={type}
       onClick={handleClick}
       className={`bg-gray-100 py-1 px-4 rounded hover:bg-gray-200 text-text-primary ${className}`}
+      disabled={disabled}
     >
       {children}
     </button>
