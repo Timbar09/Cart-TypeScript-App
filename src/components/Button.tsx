1bc2fb103c29473c192bbb20e511a0d657c36e08
Miles Mosweu <tmtmosweu@gmail.com>
2024-10-01 19:49:02 +0200

    Refactor Button component to add aria-label and title attributes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,8 @@
  * @param {string} [className=''] - Additional classes to apply to button
  * @param {string} [type='button'] - Button type attribute
  * @param {boolean} [disabled=false] - Button disabled attribute
+ * @param {string} [ariaLabel=''] - Button aria-label attribute
+ * @param {string} [title=''] - Button title attribute
  *
  * @returns {JSX.Element} - Button component
  */
@@ -15,6 +17,8 @@ type ButtonProps = {
   className?: string;
   type?: 'button' | 'submit' | 'reset';
   disabled?: boolean;
+  ariaLabel?: string;
+  title?: string;
 };
 
 const Button = ({
@@ -23,6 +27,8 @@ const Button = ({
   className = '',
   type = 'button',
   disabled = false,
+  ariaLabel = '',
+  title = '',
 }: ButtonProps): JSX.Element => {
   return (
     <button
@@ -30,6 +36,8 @@ const Button = ({
       onClick={handleClick}
       className={`bg-gray-100 py-1 px-4 rounded hover:bg-gray-200 text-text-primary ${className}`}
       disabled={disabled}
+      aria-label={ariaLabel}
+      title={title}
     >
       {children}
     </button>
