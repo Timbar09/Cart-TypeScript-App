4881a6bc4247c235f03eaa9f770cb2752033d9dd
Miles Mosweu <tmtmosweu@gmail.com>
2024-11-05 21:36:41 +0200

    Refactor ProductView component to display cart information and add wishlist functionality

--- src/components/Products/ProductView.tsx
+++ src/components/Products/ProductView.tsx
@@ -2,10 +2,18 @@ import { Link, useParams } from 'react-router-dom';
 
 import useProducts from '../../hooks/useProducts';
 import { ProductType } from '../../context/ProductsProvider';
+import useCart from '../../hooks/useCart';
+
+import { numToCurrency } from '../functions';
+import Button from '../Button';
+import CartLineItemStepper from '../Cart/CartLineItemStepper';
 
 const ProductView = () => {
+  const { dispatch, REDUCER_ACTIONS, cart } = useCart();
   const { sku } = useParams<{ sku: string }>();
   const { products } = useProducts();
+
+  const cartLineItem = cart.find((item) => item.sku === sku);
 
   const product: ProductType | undefined = products.find((product) => product.sku === sku);
   const img: string = new URL(`../../images/${product?.sku}.jpg`, import.meta.url).href;
@@ -20,12 +28,28 @@ const ProductView = () => {
       className="flex flex-col items-center justify-center gap-4 h-[calc(100vh-9rem)] min-h-[23rem]"
     >
       {product ? (
-        <div className="product relative flex flex-col items-center gap-4 max-w-sm mx-auto border-2 border-gray-100 rounded-lg p-4">
+        <div className="product relative flex flex-col items-center gap-4 max-w-80 mx-auto border-2 border-gray-100 rounded-lg py-4 px-8">
           <h1 className="text-2xl text-text-primary">{product.name}</h1>
           <img src={img} alt={product.name} className="w-100" />
 
-          <div className="left flex flex-col absolute top-[5%] bottom-[5%] right-[100%] border-2 border-gray-100 rounded-s-lg">
-            <h3 className="text-lg text-text-primary w-max p-4">Similar Products</h3>
+          {cartLineItem ? (
+            <span className="relative -bottom-8 py-2 px-6 w-max bg-primary-50 rounded">
+              Added to Cart
+            </span>
+          ) : (
+            <Button
+              buttonRole="primary"
+              type="button"
+              className="relative -bottom-8 px-6 w-max"
+              aria-label="Add to Cart"
+              title="Add to Cart"
+            >
+              Add to Cart
+            </Button>
+          )}
+
+          <div className="left text-center flex flex-col absolute top-[5%] bottom-[5%] right-[100%] border-2 border-gray-100 rounded-s-lg">
+            <h3 className="text-sm text-text-primary w-max p-4">Similar Products</h3>
 
             <ul className="flex flex-col flex-1">
               {similarProducts.map((product, i) => (
@@ -36,7 +60,7 @@ const ProductView = () => {
                       i !== products.length - 1 ? 'border-t-2 border-gray-100' : 'rounded-es-lg'
                     }`}
                   >
-                    <div className="w-16 mx-auto">
+                    <div className="w-14 mx-auto">
                       <img
                         src={new URL(`../../images/${product.sku}.jpg`, import.meta.url).href}
                         alt={product.name}
@@ -50,8 +74,39 @@ const ProductView = () => {
             </ul>
           </div>
 
-          <div className="right absolute top-[5%] bottom-[5%] left-[100%] p-4 border-2 border-gray-100 rounded-e-lg">
-            <p>{product.price}</p>
+          <div className="right flex flex-col gap-6 text-center absolute top-[5%] bottom-[5%] left-[100%] p-4 border-2 border-gray-100 rounded-e-lg">
+            <div className="">
+              <h3 className="text-sm text-text-primary mb-2">Price</h3>
+
+              <p className="text-4xl text-text-primary font-medium">
+                {numToCurrency(product.price)}
+              </p>
+            </div>
+
+            <Button
+              buttonRole="secondary"
+              type="button"
+              className="p-2 w-max"
+              aria-label="Add to Cart"
+              title="Add to wishlist"
+            >
+              Add to Wishlist
+            </Button>
+
+            {cartLineItem && (
+              <div className="flex flex-col gap-4 items-center">
+                <CartLineItemStepper
+                  item={cartLineItem}
+                  dispatch={dispatch}
+                  REDUCER_ACTIONS={REDUCER_ACTIONS}
+                  size="lg"
+                />
+
+                <p className="text-xl text-text-primary font-medium">
+                  {numToCurrency(cartLineItem.price * cartLineItem.quantity)}
+                </p>
+              </div>
+            )}
           </div>
         </div>
       ) : (
