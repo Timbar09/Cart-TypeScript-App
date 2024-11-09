a111d4b885a8fda46bc15558172e812cc57c1ff5
Miles Mosweu <tmtmosweu@gmail.com>
2024-11-09 10:49:54 +0200

    Add useWishlist hook and integrate WishlistProvider in main application

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom';
 
 import { ProductsProvider } from './context/ProductsProvider.tsx';
 import { CartProvider } from './context/CartProvider.tsx';
+import { WishlistProvider } from './context/WishlistProvider.tsx';
 
 import router from './router';
 
@@ -14,7 +15,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ProductsProvider>
       <CartProvider>
-        <RouterProvider router={router} />
+        <WishlistProvider>
+          <RouterProvider router={router} />
+        </WishlistProvider>
       </CartProvider>
     </ProductsProvider>
   </StrictMode>
