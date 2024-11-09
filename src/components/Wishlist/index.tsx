98923e59640b3e9f2986e68a3e37bb0fc8f522fe
Miles Mosweu <tmtmosweu@gmail.com>
2024-11-09 17:35:54 +0200

    Implement Wishlist component to display products and handle loading state

--- src/components/Wishlist/index.tsx
+++ src/components/Wishlist/index.tsx
@@ -1,4 +1,29 @@
+import { ReactElement } from 'react';
+
+import useWishlist from '../../hooks/useWishlist';
+
+import Product from '../Products/Product';
+
 const Wishlist = () => {
-  return <div>Wishlist</div>;
+  const { wishlist } = useWishlist();
+
+  let content: ReactElement | ReactElement[] = <div>Loading...</div>;
+
+  if (wishlist?.length) {
+    content = wishlist.map((product) => {
+      return <Product key={product.sku} product={product} />;
+    });
+  }
+
+  return (
+    <div className="@container">
+      <header className=" py-4">
+        <h2 className="text-3xl font-medium text-text-primary">Wishlist</h2>
+      </header>
+
+      <ul className="@xs:grid @xs:grid-cols-2 @xl:grid-cols-3 lg:grid-cols-4 gap-4">{content}</ul>
+    </div>
+  );
 };
+
 export default Wishlist;
